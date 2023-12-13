c27928b9bd58fddfdf46c343e66bf0b3df55c4f5
Ivan Code <ivanpaglinawan0@gmail.com>
2023-12-13 23:38:48 +0800

    Update eleporidae.sql

--- eleporidae.sql
+++ eleporidae.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2023 at 04:21 PM
+-- Generation Time: Dec 13, 2023 at 04:34 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -54,9 +54,7 @@ CREATE TABLE `adoption` (
 --
 
 INSERT INTO `adoption` (`id`, `user_id`, `rabbit_id`, `adoption_date`, `phone`, `province`, `city`, `barangay`, `reason_for_adoption`, `other_pets`, `adoption_status`, `home_environment_image_path`, `service_option`, `delivery_status`, `comment`, `price`, `mode_of_payment`, `agriculture_product`, `agriculture_product_price`) VALUES
-('0e9a0505-1a87-41fe-b976-b1e10c3cf236', '6226702d-125e-4c1d-973b-994c76bf52a3', 'a55a0de6-c9b7-4ebb-bba1-5ee445ba980c', '2023-12-04', '09483251236', 'CAMARINES SUR', 'CABUSAO', 'Santa Cruz', 'Quiet and gentle nature', '', 'Pending', 'Gantt chart - eLeporidae (2).png', 'Deliver', NULL, NULL, 300, 'Cash', '', 0),
-('311f2cf6-1697-4126-b947-859f6e44568c', '6226702d-125e-4c1d-973b-994c76bf52a3', '4bde3adb-0370-4724-9b42-25be2c25a64b', '2023-12-04', '09456516546', 'CAMARINES SUR', 'CABUSAO', 'Santa Cruz', 'Rabbits are inexpensive', '', 'Declined', 'Gantt chart - eLeporidae (2).png', 'Deliver', NULL, 'Incomplete', 350, 'Cash', '', 0),
-('327372c3-b207-4b9a-a40d-42aa8455e151', '6226702d-125e-4c1d-973b-994c76bf52a3', '27e434e0-2763-4f11-ad74-1a97b362a75a', '2023-12-04', '09482048756', 'CAMARINES SUR', 'CABUSAO', 'Santa Cruz', 'Quiet and gentle nature', '', 'Approved', 'Gantt chart - eLeporidae (2).png', 'Deliver', 'Approved', NULL, 300, 'Cash', '', 0);
+('a2147255-3da6-4249-a1b3-5c7cb64ee089', '6226702d-125e-4c1d-973b-994c76bf52a3', '5eee1b09-4841-45a9-bce2-ffc484436760', '2023-12-13', '09482048756', 'CAMARINES SUR', 'CABUSAO', 'Santa Cruz', 'Rabbits are a great pet', '', 'Approved', 'Gantt chart - eLeporidae (3).png', 'Deliver', 'Approved', NULL, 350, 'Cash', '', 0);
 
 -- --------------------------------------------------------
 
@@ -78,33 +76,18 @@ CREATE TABLE `breeding_pair` (
 --
 
 INSERT INTO `breeding_pair` (`id`, `buck_id`, `doe_id`, `note`, `pairing_date`, `expected_due_date`) VALUES
-('0a47fa92-db93-42ad-8fdd-49259036712e', 'a55a0de6-c9b7-4ebb-bba1-5ee445ba980c', 'cb9e8d58-2fd5-4921-9b75-bb47a7aa13fe', NULL, '2023-12-01', '2024-01-02'),
-('0c3bb097-ad6a-40a3-a231-5743372a9142', 'a55a0de6-c9b7-4ebb-bba1-5ee445ba980c', 'cb9e8d58-2fd5-4921-9b75-bb47a7aa13fe', NULL, '2023-11-27', '2023-12-29'),
-('1e018426-4985-4960-98f8-aff647c84ffe', 'a55a0de6-c9b7-4ebb-bba1-5ee445ba980c', 'cb9e8d58-2fd5-4921-9b75-bb47a7aa13fe', NULL, '2023-11-27', '2023-12-29'),
-('53905d68-ca1f-4d34-bcfd-a36b977d2888', 'a55a0de6-c9b7-4ebb-bba1-5ee445ba980c', 'cb9e8d58-2fd5-4921-9b75-bb47a7aa13fe', 'George and Gorgia', '2023-12-01', '2024-01-02'),
-('66ac5077-d0ee-455c-85bc-e79ec28329c5', 'a55a0de6-c9b7-4ebb-bba1-5ee445ba980c', 'cb9e8d58-2fd5-4921-9b75-bb47a7aa13fe', NULL, '2023-11-26', '2023-12-26'),
-('9cc29ecf-f319-4e4d-851c-3e5bfc354096', 'a55a0de6-c9b7-4ebb-bba1-5ee445ba980c', 'cb9e8d58-2fd5-4921-9b75-bb47a7aa13fe', NULL, '2023-11-27', '2023-12-29'),
-('c17a8594-b453-464f-9b15-bfd7d89a9f9d', 'a55a0de6-c9b7-4ebb-bba1-5ee445ba980c', 'cb9e8d58-2fd5-4921-9b75-bb47a7aa13fe', NULL, '2023-11-26', '2023-12-28');
+('dbb32ecb-5a7a-4bbe-86d2-ecc52b820c68', '40f87a68-9c15-4d08-9164-225102ce652a', '4bde3adb-0370-4724-9b42-25be2c25a64b', 'L&Ch', '2023-12-13', '2024-01-14');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `chat`
+-- Table structure for table `config`
 --
 
-CREATE TABLE `chat` (
-  `id` int(11) NOT NULL,
-  `message` varchar(255) DEFAULT NULL,
-  `user_id` int(11) NOT NULL,
-  `type` varchar(255) DEFAULT NULL
+CREATE TABLE `config` (
+  `name` varchar(255) DEFAULT NULL,
+  `value` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `chat`
---
-
-INSERT INTO `chat` (`id`, `message`, `user_id`, `type`) VALUES
-(1, 'hello world', 1, 'Admin');
 
 -- --------------------------------------------------------
 
@@ -114,6 +97,7 @@ INSERT INTO `chat` (`id`, `message`, `user_id`, `type`) VALUES
 
 CREATE TABLE `rabbit` (
   `id` varchar(255) NOT NULL,
+  `breeding_pair_id` varchar(255) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
   `date_of_birth` date DEFAULT NULL,
   `sex` varchar(255) NOT NULL,
@@ -123,20 +107,23 @@ CREATE TABLE `rabbit` (
   `weight` varchar(255) NOT NULL,
   `image_path` varchar(255) DEFAULT NULL,
   `rehome_status` varchar(255) DEFAULT NULL,
-  `price` double NOT NULL
+  `price` double NOT NULL,
+  `is_adopted` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `rabbit`
 --
 
-INSERT INTO `rabbit` (`id`, `name`, `date_of_birth`, `sex`, `rabbit_type`, `color`, `breed_type`, `weight`, `image_path`, `rehome_status`, `price`) VALUES
-('27e434e0-2763-4f11-ad74-1a97b362a75a', 'Totoro', '2022-12-06', 'Male', 'Pet rabbit', 'Brown', 'Havana', '2', 'totoro.jpg', 'Rehome', 300),
-('4bde3adb-0370-4724-9b42-25be2c25a64b', 'Lea', '2023-07-01', 'Female', 'Pet rabbit', 'Fawn', 'Flemish Giant', '2', 'Lea.jpg', 'Rehome', 350),
-('a55a0de6-c9b7-4ebb-bba1-5ee445ba980c', 'Gerald', '2023-07-03', 'Male', 'Pet rabbit', 'Sooty', 'French Lop', '2', 'Gerald.jpg', 'Rehome', 300),
-('cb9e8d58-2fd5-4921-9b75-bb47a7aa13fe', 'Georgia', '2023-06-30', 'Male', 'Pet rabbit', 'Black', 'Beveren', '1.7', 'george.jpg', 'Rehome', 300),
-('cc9d8b5f-d8dc-4928-a34c-698f44125a0e', 'Pancake', '2023-07-02', 'Female', 'Pet rabbit', 'White', 'American', '1.6', 'Pancake.jpg', 'Rehome', 0),
-('f8c9097c-5117-450e-bb69-760d3fb249dd', 'Leo', '2023-07-09', 'Male', 'Pet rabbit', 'White', 'Beveren', '2', 'leo.jpg', 'Rehome', 350);
+INSERT INTO `rabbit` (`id`, `breeding_pair_id`, `name`, `date_of_birth`, `sex`, `rabbit_type`, `color`, `breed_type`, `weight`, `image_path`, `rehome_status`, `price`, `is_adopted`) VALUES
+('40f87a68-9c15-4d08-9164-225102ce652a', NULL, 'Chingkong', '2023-12-08', 'Male', 'Pet rabbit', 'Black', 'American', '1.4', 'âPngtreeârabbit_3644555.png', NULL, 300, 0),
+('4bde3adb-0370-4724-9b42-25be2c25a64b', NULL, 'Lea', '2023-06-29', 'Female', 'Meat rabbit', 'Tan', 'Flemish Giant', '2', 'Lea.jpg', NULL, 350, 0),
+('5eee1b09-4841-45a9-bce2-ffc484436760', '5f858901-5391-4860-bd5c-06e7337ce8dc', 'AME-BEV', '2023-12-11', 'Male', 'Pet rabbit', 'Fawn', 'American-Beveren', '2', 'Lea.jpg', 'Rehome', 350, 1),
+('982878d3-399d-41d9-95e8-5524e16a1393', '97f53b50-e34b-4011-a2b2-a382ee5e4db0', 'Akif', '2023-12-09', 'Male', 'Pet rabbit', 'Tan', 'French Lop-Flemish Giant', '1.2', 'harold.jpg', 'Rehome', 350, 0),
+('a55a0de6-c9b7-4ebb-bba1-5ee445ba980c', NULL, 'Gerald', '2023-07-02', 'Male', 'Pet rabbit', 'Sooty', 'French Lop', '2', 'Gerald.jpg', 'Rehome', 300, 0),
+('cb9e8d58-2fd5-4921-9b75-bb47a7aa13fe', NULL, 'Georgia', '2023-06-27', 'Female', 'Pet rabbit', 'Black', 'Beveren', '1.7', 'george.jpg', 'Rehome', 300, 0),
+('cc9d8b5f-d8dc-4928-a34c-698f44125a0e', NULL, 'Pancake', '2023-07-02', 'Female', 'Pet rabbit', 'White', 'American', '1.6', 'Pancake.jpg', 'Rehome', 400, 0),
+('f8c9097c-5117-450e-bb69-760d3fb249dd', NULL, 'Leo', '2023-07-09', 'Male', 'Pet rabbit', 'White', 'Beveren', '2', 'leo.jpg', 'Rehome', 350, 0);
 
 -- --------------------------------------------------------
 
@@ -156,10 +143,7 @@ CREATE TABLE `transaction` (
 --
 
 INSERT INTO `transaction` (`id`, `adoption_id`, `transaction_date`, `transaction_status`) VALUES
-('3eb2c70f-34a9-42ce-b09e-eadd89bbbd76', 'fe8834c1-ca6b-48a5-84e4-791b1f40c815', '2023-12-02', 'Completed'),
-('94c3b646-0dfb-4b18-b2b2-0784160e5dc4', '0e6485ad-5341-46b3-b516-cfe76b326e07', '2023-12-02', 'Completed'),
-('a91bcb07-dd0e-41d6-8081-ed1d17916748', '11aca217-d7aa-48a5-9474-c361109eca3e', '2023-12-02', 'Completed'),
-('eb03ed08-1499-4991-86c3-d8227af1d097', '0a63e94d-c92b-435c-be19-c4c2daa387fe', '2023-12-02', 'Completed');
+('08dc8995-9df1-48f7-ba1b-c87285bd81d1', 'a2147255-3da6-4249-a1b3-5c7cb64ee089', '2023-12-13', 'Completed');
 
 -- --------------------------------------------------------
 
@@ -184,13 +168,10 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `name`, `email`, `password`, `user_type`, `profile`, `otp`, `is_verified`) VALUES
 ('6226702d-125e-4c1d-973b-994c76bf52a3', 'Ivan Bengcolado', 'ivanpaglinawan0@gmail.com', '1234', 'client', 'âPngtreeârabbit_3644555.png', 5420, 1),
-('81a1feee-b100-4750-850c-1ff3cdba7676', 'Ivan', 'ivanbengcolado@gmail.com', '1234', 'client', NULL, 2741, 0),
-('d09a9d2d-0984-4fda-8476-52a56a6cf484', 'Leonardo Nogra', 'leonardo@gmail.com', 'admin', 'admin', 'IMG_20190918_171058.jpg', NULL, 0),
-('d68cd68d-887f-4896-a46c-88a639550cc7', 'Ivan Paglinawan', 'ivanbeng@gmail.com', '1234', 'admin', NULL, NULL, 0),
+('d09a9d2d-0984-4fda-8476-52a56a6cf484', 'Leonardo Nogra', 'leonardo@gmail.com', 'admin', 'admin', NULL, NULL, 0),
 ('dc63f09f-f4c6-4bce-b73b-f83d64746ee1', 'Ivan', 'ivanpaglinawan00@gmail.com', 'zsdcas', 'client', NULL, NULL, 0),
-('f0a9b912-a774-4219-badd-c0a9c5317d5d', 'Francis Bawag', 'francisbawag@gmail.com', '1234', 'client', NULL, NULL, 0),
-('f335a672-864a-40e5-9158-1c8e32fd7c10', 'Lorio', 'lorioj@gmail.com', '1234', 'client', NULL, 5307, 0),
-('f75cbbc5-6479-4add-83e2-6ca52e93f882', 'Cynthia Lorio', 'cynthialorio@gmail.com', '1234', 'client', NULL, NULL, 0);
+('effbe5b7-9757-4bfb-b6b9-2e5f170c6787', 'Cynthia Lorio', 'cynthialorio08@gmail.com', '1234', 'client', NULL, 7856, 1),
+('f0a9b912-a774-4219-badd-c0a9c5317d5d', 'Francis Bawag', 'francisbawag@gmail.com', '1234', 'client', NULL, NULL, 1);
 
 --
 -- Indexes for dumped tables
@@ -206,12 +187,6 @@ ALTER TABLE `adoption`
 -- Indexes for table `breeding_pair`
 --
 ALTER TABLE `breeding_pair`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `chat`
---
-ALTER TABLE `chat`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -231,16 +206,6 @@ ALTER TABLE `transaction`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `chat`
---
-ALTER TABLE `chat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
