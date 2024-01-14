5b444219133528718ae652de517a3861cd4f548b
Aldrin Aureus <99024862+Trink3t@users.noreply.github.com>
2024-01-15 01:25:37 +0800

    Fixed some error

--- admin/src/controller/UserController.jsx
+++ admin/src/controller/UserController.jsx
@@ -1,11 +1,10 @@
 import appConfig from '../../config.json';
-const BASE_URL = appConfig.apiBasePath; //e.g "http://localhost:8080/api";
 import SecureStore from "react-secure-storage";
-import.meta.env;
 
+const BASE_URL = appConfig.apiBasePath; //e.g "http://localhost:8080/api";
+var END_POINT = "/login";
 //for experimental
 export async function signin(data) {
-  var END_POINT = "/login";
   console.log("siginin", BASE_URL + END_POINT);
   var response = await fetch(BASE_URL + END_POINT, {
     method: "POST",
